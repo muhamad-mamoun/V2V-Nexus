8e8cea553404e09177fbfe3819cb460177932465
Muhamad Mamoun <muhamadmamoun@gmail.com>
2023-12-15 13:35:27 +0200

    Fix core ECU issues

--- V2V-coreECU/Keil/Objects/startup_stm32f303xe.d
+++ V2V-coreECU/Keil/Objects/startup_stm32f303xe.d
@@ -1,1 +1,1 @@
-.\objects\startup_stm32f303xe.o: RTE\Device\STM32F303RETx\startup_stm32f303xe.s
+.\objects\startup_stm32f303xe.o: RTE/Device/STM32F303RETx/startup_stm32f303xe.s
--- V2V-coreECU/Keil/Objects/system_stm32f3xx.d
+++ V2V-coreECU/Keil/Objects/system_stm32f3xx.d
@@ -1,10 +1,10 @@
 ./objects/system_stm32f3xx.o: RTE\Device\STM32F303RETx\system_stm32f3xx.c \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f3xx.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f303xe.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f3xx.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f303xe.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\sharb\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\system_stm32f3xx.h
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\Mamoun\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\system_stm32f3xx.h
